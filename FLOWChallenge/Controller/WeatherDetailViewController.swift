ee2bc20db72aa3f7dce244375aedbd05916bf87d
Maidana Rodrigo <maidanarodrigo.dev@gmail.com>
2022-09-14 19:04:45 -0700

    Setup Project Structure and Storyboard

---
+++ FLOWChallenge/Controller/WeatherDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDetailViewController.swift
+//  FLOWChallenge
+//
+//  Created by Rodrigo Maidana on 14/09/2022.
+//
+
+import Foundation
+import UIKit
+
+class WeatherDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ FLOWChallenge/Controller/WeatherViewController.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherViewController.swift
+//  FLOWChallenge
+//
+//  Created by Rodrigo Maidana on 14/09/2022.
+//
+
+import Foundation
+import UIKit
+
+class WeatherViewController: UIViewController {
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var descrptionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
